063ef7bd801c9cfe60eb2275a4d517d33d5a33f4
unknown <prides.ykt@gmail.com>
2019-01-26 16:53:32 +0900

    some changes

--- Project/VRTest002/Assets/Scripts/StageState.cs
+++ Project/VRTest002/Assets/Scripts/StageState.cs
@@ -18,13 +18,13 @@ public class StageState : MonoBehaviour {
   public event EventHandler onTimeOverEvent;
   public event EventHandler onTimeChangeEvent;
 
-  public List<PickupableItem> grassRollList;
+  public List<Triggerable> grassRollList;
 
   void Start() {
     currentTime = startTime;
 
-    foreach(PickupableItem grassRoll in grassRollList) {
-      grassRoll.onPickUpEvent += OnGrassRollReceive;
+    foreach(Triggerable grassRoll in grassRollList) {
+      grassRoll.onTriggerEvent += OnGrassRollReceive;
     }
   }
 
@@ -39,9 +39,9 @@ public class StageState : MonoBehaviour {
     }
   }
 
-  private void OnGrassRollReceive(PickupableItem sender, GameObject reciever) {
+  private void OnGrassRollReceive(Triggerable sender, Collider reciever) {
     Destroy(sender.gameObject);
-    currentTime += 10;
+    //currentTime += 10;
     grassRollList.Remove(sender);
   }
 }
