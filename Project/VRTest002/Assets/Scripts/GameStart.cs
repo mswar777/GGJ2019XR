594b4a66b306575e067fca881659a1ea2a1e1145
hakase0274 <hakase0274@gmail.com>
2019-01-26 20:56:55 +0900

    UIをVRで調整 シーン遷移機構実装(他の環境で要テスト)

---
+++ Project/VRTest002/Assets/Scripts/GameStart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour
+{
+    //開始時に表示するテキスト
+    public Text startText;
+    //テキストがフェードして消える時間
+    public int fadeTime;
+    //アルファ値が下がる量
+    private float alpha;
+    
+    private void Start()
+    {
+        if (startText == null) Destroy(this);
+        alpha = startText.color.a / (fadeTime  * 60);
+        StartCoroutine(TextFadeCoroutine());
+    }
+
+    /// <summary>
+    /// テキストをフェードさせるコルーチン
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TextFadeCoroutine()
+    {
+        while(startText.color.a > 0.0f)
+        {
+            var color = startText.color;
+            color.a -= alpha;
+            startText.color = color;
+            yield return null;
+        }
+    }
+}
---
+++ Project/VRTest002/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+public enum SceneState
+{
+    TITLE_SCENE = 0,
+    GAME_SCENE = 1,
+    ENDING_SCENE = 2,
+    GAME_OVER_SCENE = 3,
+    TEST_SCENE = 5,
+    TEST_STAGE = 4,
+}
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneWithFade(SceneState scene)
+    {
+        var sceneName = "";
+        switch (scene)
+        {
+            case SceneState.TITLE_SCENE:
+                sceneName = "SceneTitle";
+                break;
+            case SceneState.GAME_SCENE:
+                sceneName = "SceneGame";
+                break;
+            case SceneState.ENDING_SCENE:
+                sceneName = "SceneEnding";
+                break;
+            case SceneState.GAME_OVER_SCENE:
+                sceneName = "SceneGameOver";
+                break;
+            case SceneState.TEST_SCENE:
+                sceneName = "TestScene";
+                break;
+            case SceneState.TEST_STAGE:
+                sceneName = "TestStage";
+                break;
+        }
+        SteamVR_LoadLevel.Begin(sceneName);
+    }
+
+    public void LoadScene(SceneState scene)
+    {
+        var sceneName = "";
+        switch (scene)
+        {
+            case SceneState.TITLE_SCENE:
+                sceneName = "SceneTitle";
+                break;
+            case SceneState.GAME_SCENE:
+                sceneName = "SceneGame";
+                break;
+            case SceneState.ENDING_SCENE:
+                sceneName = "SceneEnding";
+                break;
+            case SceneState.GAME_OVER_SCENE:
+                sceneName = "SceneGameOver";
+                break;
+            case SceneState.TEST_SCENE:
+                sceneName = "TestScene";
+                break;
+            case SceneState.TEST_STAGE:
+                sceneName = "TestStage";
+                break;
+        }
+        SceneManager.LoadSceneAsync(sceneName,LoadSceneMode.Additive);
+    }
+}
